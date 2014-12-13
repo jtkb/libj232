7b028b1edd5150495e750525c225afdf0be0b6e9
Kerry Billingham <java@avionicengineers.com>
2014-12-13 17:04:19 +0000

    Introduced a new function that converts Java-define flag values to their equivalent native-define values.

--- src/serial.c
+++ src/serial.c
@@ -23,9 +23,11 @@
 JNIEXPORT jint JNICALL
 Java_com_javatechnics_rs232_Serial_openSerialPort (JNIEnv * env, jobject obj, jstring path, jint flags){
 	jint return_value = -1;
+        int native_flags = get_real_flags(open_flags, universal_flag_array, \
+                                            flags, number_open_flags);
         const jbyte *c_path = (*env)->GetStringUTFChars(env, path, NULL);
         if (c_path != NULL){
-            return_value = open(c_path, flags);
+            return_value = open(c_path, native_flags);
             if (return_value == -1){
                 jint error = errno; //preserve the correct error number
                 // Throw exception
@@ -69,4 +71,25 @@ Java_com_javatechnics_rs232_Serial_closeSerialPort (JNIEnv *env, jobject obj, ji
         }
     }
     return return_value;
+}
+
+/**
+ *  This is a helper function that converts Java-defined flag values to their
+ * equivalent in native code and returns a native flag value.
+ * @param java_flags An array of the flag values as defined in the Java code.
+ * @param native_flags An array of the equivalent native flags.
+ * @param selected_flags An integer value of the required Java-defined flags.
+ * @param size The size of the java_flags array.
+ * @return The converted Java-to-Native flags.
+ */
+int get_real_flags(const int java_flags[], \
+                    const int native_flags[], \
+                    const int selected_flags, \
+                    const int size){
+    int return_flag = 0, count = 0;
+    for (; count < size; count++){
+        if (selected_flags & java_flags[count])
+            return_flag |= native_flags[count];
+    }
+    return return_flag;
 }
--- src/serial.h
+++ src/serial.h
@@ -1,6 +1,7 @@
 #ifndef _SERIAL_H
 #define _SERIAL_H
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -11,5 +12,20 @@
 #include <jni.h>
 #include "jni/com_javatechnics_rs232_Serial.h"
 
+const int universal_flag_array[] = {0x00001, 0x00002, 0x00004, 0x00008, \
+                        0x00010, 0X00020, 0X00040, 0X00080, \
+                        0X00100, 0X00200, 0X00400, 0X00800, \
+                        0X01000, 0X02000, 0X04000, 0X08000, \
+                        0X10000, 0X20000};
+
+const int open_flags[] = { O_APPEND, O_ASYNC, O_CLOEXEC, O_CREAT, \
+                            O_DIRECT, O_DIRECTORY, O_DSYNC, O_EXCL,
+                            O_LARGEFILE, O_NOATIME, O_NOCTTY, O_NOFOLLOW, \
+                            O_NONBLOCK, O_SYNC, O_TRUNC, O_RDONLY, \
+                            O_WRONLY, O_RDWR };
+const int number_open_flags = sizeof(open_flags) / sizeof(open_flags[0]);
+
+int get_real_flags(const int java_flags[], const int native_flags[], \
+                            const int selected_flags, const int size);
 
 #endif
